ed2b34c88987a352202d4ce7570b31d7fd55944f
Hariharan Subramanian <hari@flowinsights.com>
2023-04-01 22:09:56 +0200

    Created login callback and routing to home page on successful login or register

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,8 +1,12 @@
 import Input from "@/components/Input";
 import {useCallback, useState} from "react";
 import axios from "axios";
+import {signIn} from 'next-auth/react'
+import {useRouter} from "next/router";
 
 const Auth = () => {
+    const router = useRouter()
+
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
@@ -13,6 +17,20 @@ const Auth = () => {
         setVariant(currVariant => currVariant === 'login' ? 'register' : 'login')
     }, []);
 
+    const login = useCallback(async () => {
+        try {
+            await signIn('credentials', {
+                email,
+                password,
+                redirect: false,
+                callbackUrl: '/'
+            });
+            router.push('/')
+        } catch (error) {
+            console.log(error)
+        }
+    }, [email, password, router]);
+
     const register = useCallback(async () => {
         try {
             await axios.post('/api/register', {
@@ -20,10 +38,12 @@ const Auth = () => {
                 name,
                 password
             })
+
+            login()
         } catch (error) {
             console.log(error)
         }
-    }, [email, name, password]);
+    }, [email, name, password, login]);
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -62,7 +82,7 @@ const Auth = () => {
                             />
                         </div>
                         <button
-                            onClick={register}
+                            onClick={variant === 'login' ? login : register}
                             className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                             {variant === 'login' ? 'Login' : 'Sign up'}
                         </button>
