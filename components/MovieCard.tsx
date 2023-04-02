fec91671deae379c568a052257cc567b42584c03
Hariharan Subramanian <hari@flowinsights.com>
2023-04-02 17:08:44 +0200

    Opening and closing info modal using zustand state

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 
 import {BsFillPlayFill} from "react-icons/bs";
+import {BiChevronDown} from "react-icons/bi";
 import FavouriteButton from "@/components/FavouriteButton";
 import {useRouter} from "next/router";
+import useInfoModal from "@/hooks/useInfoModal";
 
 interface MovieCardProps {
     data: Record<string, any>
@@ -10,6 +12,7 @@ interface MovieCardProps {
 
 const MovieCard: React.FC<MovieCardProps> = ({data}) => {
     const router = useRouter()
+    const {openModal} = useInfoModal()
 
     return (
         <div className="group bg-zinc-900 col-span relative h-[12vw]">
@@ -27,6 +30,11 @@ const MovieCard: React.FC<MovieCardProps> = ({data}) => {
                             <BsFillPlayFill size={30}/>
                         </div>
                         <FavouriteButton movieId={data?.id}/>
+                        <div
+                            onClick={() => openModal(data?.id)}
+                            className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
+                            <BiChevronDown className="text-white group-hover/item:text-neutral-300" size={30}/>
+                        </div>
                     </div>
 
                     <p className="text-green-400 font-semibold mt-4">
