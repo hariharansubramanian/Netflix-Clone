be1ec1567c0e6207b13b71add27fc6b599150ad9
Hariharan Subramanian <hari@flowinsights.com>
2023-04-02 15:10:34 +0200

    Created Billboard with a random movie

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import {getSession} from "next-auth/react";
 import {NextPageContext} from "next";
 import Navbar from "@/components/Navbar";
+import Billboard from "@/components/Billboard";
 
 export async function getServerSideProps(context: NextPageContext) {
     const session = await getSession(context)
@@ -23,6 +24,7 @@ export default function Home() {
     return (
         <>
             <Navbar/>
+            <Billboard/>
         </>
     )
 }
